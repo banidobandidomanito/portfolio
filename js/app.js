2082f5694b07fce2df55f344f9bfe0ecd8ca1619
banidobandidomanito <joaoaugustoalez64@gmail.com>
2024-07-02 01:42:33 -0400

    molde do site completo

--- js/app.js
+++ js/app.js
@@ -1,22 +1,8 @@
-{/* {<button class="rodape__button" id="theme-button" onclick="changeTheme()"><img src="./assets/icons/ico-changetheme.svg" alt="mode" width="32px"></button> */}
-
-
-let dark = document.getElementById('theme').href = "./css/dark.css";
-let light = document.getElementById('theme').href = "./css/style.css";
-
-// function changeTheme(){
-//     if (document.getElementById('theme').href == dark){
-//         document.getElementById('theme').href = light;
-//     }
-//     else{
-//         document.getElementById('theme').href = dark;
-//     }
-// }
-
-function changeTheme(){
-    document.getElementById('theme').href = dark;
-    if (document.getElementById('theme').href == dark){
-        document.getElementById('theme').href = light;
-    }
-    document.getElementById('icon-home').fill='green';
+function opa(){
+    document.getElementById('ibagem').src="./assets/images/gatojoinha.jpg";
+    audio();
+}
+function audio(){
+    audio = new Audio('./assets/sons/easteregg.mp3');
+    audio.play();
 }
